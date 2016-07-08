2f1919381ac61d180c34d232ef1ce4113f7beee1
Barry Williams <barry.e.williams42@gmail.com>
2016-07-07 22:23:52 -0600

    added auth header behavior, response codes, and facebook login

--- main.go
+++ main.go
@@ -3,26 +3,66 @@ package main
 import (
 	"net/http"
 	"fmt"
-	"html"
 	"log"
 	"io/ioutil"
 )
 
 func main() {
 	http.HandleFunc("/alive", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		GiveResponseFile("responses/GET_alive.txt",w)
 	})
 
-	http.HandleFunc("/api/sales", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		json, err := ioutil.ReadFile("static_content/list_sales.json")
-		if err != nil {
-			panic("reading sample html file failed")
+	// List Sales
+	http.HandleFunc("/api/sales", func(w http.ResponseWriter, r *http.Request){
+		if r.Method != "GET" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Fprintf(w, "405 method not allowed")
+			return
 		}
-		fmt.Fprintf(w, string(json))
+
+		if r.Header.Get("AuthToken") == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprintf(w, "401 unauthorized")
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		GiveResponseFile("responses/GET_api_sales.json",w)
+	})
+
+	// Facebook Login
+	http.HandleFunc("/api/login", func(w http.ResponseWriter, r *http.Request){
+		if r.Method != "POST" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Fprintf(w, "405 method not allowed")
+			return
+		}
+
+		if r.Header.Get("FacebookToken") == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "400 bad request")
+			return
+		}
+
+		if r.Header.Get("FacebookToken") == "bad_token" {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprintf(w, "401 unauthorized")
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		GiveResponseFile("responses/POST_api_login.json",w)
 	})
 
 	http.Handle("/", http.FileServer(http.Dir("../ui-frontend/src")))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func GiveResponseFile(filename string, w http.ResponseWriter){
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(fmt.Sprintf("reading sample file (%s)failed", filename))
+	}
+	fmt.Fprintf(w, string(file))
 }
